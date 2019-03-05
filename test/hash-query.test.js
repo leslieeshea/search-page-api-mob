00d486e9cbb46208675ba2bcfda91d8cc72cbcb9
Leslie Shea <leslieyshea@gmail.com>
2019-03-05 13:27:00 -0800

    wrote writePageToQuery function/test/passing

--- test/hash-query.test.js
+++ test/hash-query.test.js
@@ -9,6 +9,13 @@ function writeSearchToQuery(existingQuery, searchTerm) {
 
     return searchParams.toString();
 }
+function writePageToQuery(existingQuery, page) {
+    const searchParams = new URLSearchParams(existingQuery);
+
+    searchParams.set('page', page);
+
+    return searchParams.toString();
+}
 
 test('add search to empty query', assert => {
     //arrange
@@ -17,6 +24,28 @@ test('add search to empty query', assert => {
     //act
     const query = writeSearchToQuery(existingQuery, searchTerm);
     const expected = 'searchTerm=star+wars&page=1';
+    //assert
+    assert.equal(query, expected);
+});
+
+test('add search existing query changes search and resets page', assert => {
+    //arrange
+    const existingQuery = 'searchTerm=star+wars&page=2';
+    const searchTerm = 'harry potter';
+    //act
+    const query = writeSearchToQuery(existingQuery, searchTerm);
+    const expected = 'searchTerm=harry+potter&page=1';
+    //assert
+    assert.equal(query, expected);
+});
+
+test('write page to existing query', assert => {
+    //arrange
+    const existingQuery = 'searchTerm=harry+potter&page=1';
+    const page = 3;
+    //act
+    const query = writePageToQuery(existingQuery, page);
+    const expected = 'searchTerm=harry+potter&page=3';
     //assert
     assert.equal(query, expected);
 });
