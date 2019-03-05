110aed0a1cd82cbd230b9244249aa3c802c29d1a
Leslie Shea <leslieyshea@gmail.com>
2019-03-05 14:25:41 -0800

    added event listener to search form

--- src/search-component.js
+++ src/search-component.js
@@ -1,3 +1,4 @@
+import { writeSearchToQuery } from './query-component.js';
 const searchForm = document.getElementById('search');
 
 searchForm.addEventListener('submit', event => {
@@ -5,9 +6,15 @@ searchForm.addEventListener('submit', event => {
     
     const formData = new FormData(searchForm);
     const searchInput = formData.get('search-input');
-    const url = new URL('https://api.themoviedb.org/3/search/movie');
-    url.searchParams.set('query', searchInput);
-    url.searchParams.set('api-key', 'cb74bb60617505504abd12bd45490b45');
-    console.log(url);
+    //gets rid of hash
+    const existingQuery = window.location.hash.slice(1);
+
+    const newQuery = writeSearchToQuery(existingQuery, searchInput);
+
+    window.location.hash = newQuery;
+    // const url = new URL('https://api.themoviedb.org/3/search/movie');
+    // url.searchParams.set('query', searchInput);
+    // url.searchParams.set('api-key', 'cb74bb60617505504abd12bd45490b45');
+    // console.log(url);
     
 });
