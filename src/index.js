45305e88ea4382d38af4fd61cb6835115229fb70
Leslie Shea <leslieyshea@gmail.com>
2019-03-06 09:58:26 -0800

    added paging-component and event listeners for buttons

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './search-component.js';
 import { readFromQuery } from './query-component.js';
 import { updateSearchInput } from './search-component.js';
 import makeSearchMovieUrl from './make-search-movie-url.js';
+import './paging-component.js';
 
 window.addEventListener('hashchange', () => {
     const query = window.location.hash.slice(1);
